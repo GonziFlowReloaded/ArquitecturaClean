b4220a86e138cf5827b6fc11951353a1f83b1a1b
Jose Gonzalo Scali <gonzalojoses@hotmail.com>
2023-10-08 14:40:45 -0300

    asd

--- Cliente/interfaz/login.py
+++ Cliente/interfaz/login.py
@@ -2,8 +2,8 @@
 def mostrar_iniciar_sesion():
     print("*"*50)
     print("Login")
-    user = input("Ingrese su usuario: ")
+    user = input("Ingrese su dni: ")
     password = input("Ingrese su contraseña: ")
 
 
-    
+
--- Cliente/interfaz/register.py
+++ Cliente/interfaz/register.py
@@ -1,13 +1,21 @@
+from consultas.con_register import registro
+from interfaz.principal import mostrar_principal
 
 def mostrar_registrarse():
     print("*"*50)
     print("Registrarse")
-    user = input("Ingrese su usuario: ")
+    dni = input("Ingrese su dni: ")
+    nombre = input("Ingrese su nombre: ")
     password = input("Ingrese su contraseña: ")
     password2 = input("Ingrese nuevamente su contraseña: ")
 
     if password == password2:
-        print("Usuario registrado con éxito")
+        if registro(dni, password, nombre):
+            print("Usuario registrado con éxito")
     else:
         print("Las contraseñas no coinciden")
-        mostrar_registrarse()
+        salir = input("¿Desea salir? (S/N): ")
+        if salir == "S":
+            mostrar_principal()
+        else:
+            mostrar_registrarse()
